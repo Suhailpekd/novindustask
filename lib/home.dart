bd162130d75a79303f7db87ce7a2e49b8fe707a5
Suhail <suhailpekd@gmail.com>
2024-03-14 06:48:38 +0530

    5

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,3 @@
-import 'package:ayurvedha/classes/button.dart';
-import 'package:ayurvedha/classes/textbox.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -13,89 +11,120 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () {},
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {},
+        ),
+        actions: [
+          Stack(
+            children: [
+              IconButton(
+                icon: Icon(
+                  Icons.notifications_outlined,
+                  size: 36,
+                ),
+                onPressed: () {},
+              ),
+              Positioned(
+                right: 8.0,
+                top: 8.0,
+                child: CircleAvatar(
+                  radius: 6.0,
+                  backgroundColor: Colors.red,
+                ),
+              ),
+            ],
           ),
-          actions: [
-            Stack(
+        ],
+      ),
+      body: ListView(
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Container(
+                  height: 48,
+                  width: MediaQuery.of(context).size.width / 1.6,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    border: Border.all(
+                      color: Color.fromARGB(255, 237, 233, 233),
+                      width: 2,
+                    ),
+                  ),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 12),
+                        child: Icon(Icons.search, color: Colors.grey),
+                      ),
+                      Expanded(
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Search...',
+                            hintStyle: TextStyle(color: Colors.grey),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 12, right: 12),
+                child: Container(
+                  child: Center(
+                    child: Text(
+                      "Search",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  height: 50,
+                  width: MediaQuery.of(context).size.width / 4,
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(238, 2, 89, 48),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                IconButton(
-                  icon: Icon(
-                    Icons.notifications_outlined,
-                    size: 36,
+                Text("Sort by:"),
+                Container(
+                  child: Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text(
+                          "Date",
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        Icon(Icons.arrow_drop_down)
+                      ],
+                    ),
                   ),
-                  onPressed: () {},
-                ),
-                Positioned(
-                  right: 8.0,
-                  top: 8.0,
-                  child: CircleAvatar(
-                    radius: 6.0,
-                    backgroundColor: Colors.red,
+                  height: 50,
+                  width: MediaQuery.of(context).size.width / 2.5,
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 1, color: Colors.grey),
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
                   ),
-                ),
+                )
               ],
             ),
-          ],
-        ),
-        body: ListView(
-          children: [
-            // Row(
-            //   children: [
-            //     Padding(
-            //       padding: const EdgeInsets.only(left: 20),
-            //       child: Container(
-            //         height: 48,
-            //         width: MediaQuery.of(context).size.width / 1.6,
-            //         decoration: BoxDecoration(
-            //           borderRadius: BorderRadius.circular(12),
-            //           color: Color.fromARGB(255, 248, 243, 243),
-            //           border: Border.all(
-            //             color: Color.fromARGB(255, 237, 233, 233),
-            //             width: 2,
-            //           ),
-            //         ),
-            //         child: Row(
-            //           children: [
-            //             Padding(
-            //               padding: const EdgeInsets.symmetric(horizontal: 12),
-            //               child: Icon(Icons.search,
-            //                   color: Colors.grey), // Adjust color as needed
-            //             ),
-            //             Expanded(
-            //               child: TextFormField(
-            //                 decoration: InputDecoration(
-            //                   border: InputBorder.none,
-            //                   hintText: 'Search...',
-            //                   hintStyle: TextStyle(color: Colors.grey),
-            //                 ),
-            //               ),
-            //             ),
-            //           ],
-            //         ),
-            //       ),
-            //     ),
-            //     Padding(
-            //       padding: const EdgeInsets.only(left: 12, right: 12),
-            //       child: Container(
-            //         child: Center(
-            //             child: Text(
-            //           "Search",
-            //           style: TextStyle(color: Colors.white),
-            //         )),
-            //         height: 50,
-            //         width: MediaQuery.of(context).size.width / 4,
-            //         decoration: BoxDecoration(
-            //           color: Color.fromARGB(238, 2, 89, 48),
-            //           borderRadius: BorderRadius.circular(15),
-            //         ),
-            //       ),
-            //     ),
-            //   ],
-            // )
-          ],
-        ));
+          )
+        ],
+      ),
+    );
   }
 }
